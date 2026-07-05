0fc7098f5eb15a218ce1535b0d2a9333b1fe50fb
Frank Bari Davinson Egoavil <fdavinsonegoavil@frba.utn.edu.ar>
2026-07-05 16:56:44 -0300

    Fix: Subiendo archivo yml guardado con la ruta especifica

--- api_testing/test/test_demo.py
+++ api_testing/test/test_demo.py
@@ -3,4 +3,4 @@ import pytest_check as check
 def test_verificacion_entorno():
     # Una prueba simple para comprobar que el CI funciona
     valor_esperado = True
-    check.equal(valor_esperado, True, "El entorno de pruebas funciona correctamente")
+    check.equal(valor_esperado, True, "El entorno de pruebas funciona correctamente")   
