aa67ce8d0203707782b9478f082ab970dd5fd6d6
Frank Bari Davinson Egoavil <fdavinsonegoavil@frba.utn.edu.ar>
2026-07-05 19:47:37 -0300

    Se agrega agrega tiempos muertos en el flujo de compras

--- tests/test_add_and_buy_items.py
+++ tests/test_add_and_buy_items.py
@@ -2,7 +2,8 @@ import pytest
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 from pages.cart_page import CartPage           
-from pages.check_page import CheckPage          
+from pages.check_page import CheckPage         
+import time 
 
 def test_add_items_to_cart(driver):
     login_page = LoginPage(driver)
@@ -13,16 +14,21 @@ def test_add_items_to_cart(driver):
 
     login_page.load()
     login_page.login("standard_user", "secret_sauce")
+    time.sleep(0.5)
     inventory_page.is_loaded()
     inventory_page.add_backpack_to_cart()
     inventory_page.add_bike_light_to_cart()
     inventory_page.add_bolt_t_shirt_to_cart()
     inventory_page.go_to_cart()
+    time.sleep(0.5)
     cart_page.is_loaded()
     cart_page.click_checkout()
+    time.sleep(0.5)
     checkout_page.is_loaded()
     checkout_page.completar_informacion_checkout("Jorge", "Ruiz", "12345")
+    time.sleep(0.5)
     checkout_page.click_finish()
+    time.sleep(0.5)
     checkout_page.back_to_home()
 
     
