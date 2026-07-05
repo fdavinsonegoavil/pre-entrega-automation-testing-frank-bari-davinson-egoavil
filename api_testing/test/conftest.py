abed7a0fdb5ece2ca6ad8801eb27e80a12dc627c
Frank Bari Davinson Egoavil <fdavinsonegoavil@frba.utn.edu.ar>
2026-07-05 17:47:16 -0300

    Agrego las pruebas de los metodos post y put

--- api_testing/test/conftest.py
+++ api_testing/test/conftest.py
@@ -14,3 +14,12 @@ def post_data():
         "body": "Estructura del framework de APIs con Pytest",
         "userId": 12
     }
+
+@pytest.fixture
+def post_data_actualizado():
+    """Fixture que retorna los datos de prueba dinámicos para el PUT"""
+    return {
+        "title": "Entrega Final QA Automation - Actualizado",
+        "body": "Estructura del framework de APIs con Pytest - Actualizado",
+        "userId": 12
+    }
--- api_testing/test/test_api.py
+++ api_testing/test/test_api.py
@@ -32,3 +32,24 @@ def test_eliminar_post(post_page):
     response = post_page.delete_post(id_a_eliminar)
     
     check.equal(response.status_code, 200, f"Código esperado 200, se obtuvo {response.status_code}")
+
+def test_actualizar_post_existente(post_page, post_data_actualizado):
+    """Prueba el método PUT usando los datos del nuevo fixture de conftest"""
+    id_a_modificar = 1
+    
+    # Invocamos el método PUT de tu Page Object pasando el ID y los datos del nuevo fixture
+    response = post_page.update_post(
+        post_id=id_a_modificar,
+        title=post_data_actualizado["title"],
+        body=post_data_actualizado["body"],
+        user_id=post_data_actualizado["userId"]
+    )
+    
+    # 1. Validamos que la API responda 200 OK (código típico de actualización exitosa)
+    check.equal(response.status_code, 200, f"Código esperado 200, se obtuvo {response.status_code}")
+    
+    # 2. Convertimos a JSON y validamos que el servidor devuelva los datos modificados
+    res_json = response.json()
+    check.equal(res_json["title"], post_data_actualizado["title"], "El título no se actualizó correctamente")
+    check.equal(res_json["body"], post_data_actualizado["body"], "El body no se actualizó correctamente")
+    check.equal(res_json["id"], id_a_modificar, "El ID del recurso cambió y debería mantenerse")
